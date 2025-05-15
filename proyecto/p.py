2a7d61cdb4ed0a8978d83a97e3677980dd207113
Juan Diego Valencia Alomia <110737499+Judival30@users.noreply.github.com>
2025-05-15 16:16:03 -0500

    Add files via upload

--- proyecto/p.py
+++ proyecto/p.py
@@ -3,45 +3,55 @@ from sys import stdin
 dp = {}
 but = []
 n, m = 0, 0
-S = 0
+
 
 def manhattan(a, b):
     return abs(a[0] - b[0]) + abs(a[1] - b[1])
 
-def solve(r1, r2, t1, t2):
-    ans = float("inf")
-    act = max(r1, r2)
-    k = (r1, r2, t1, t2)
+
+def solve(r1, r2, t):
+    if r1 < r2:
+        r1, r2 = r2, r1 
+    nb = r1 + 1
+    k = (r1, r2, t)
+    ans = None
     if k in dp:
         ans = dp[k]
-        print("solpao", k)
-    elif act == len(but) - 1:
-        ans = max(t1 + manhattan(but[r1], but[act]), t2 + manhattan(but[r2], but[act]))
+    elif r1 == len(but) - 1:
+        d = manhattan(but[r1], but[r2])
+        #ans = max(0, d - t) + 1
+        ans = d - t if d - t > 0 else 0
+        ans += 1
     else:
-        d1, d2 = manhattan(but[r1], but[act + 1]), manhattan(but[r2], but[act + 1])
-        nt1, nt2 = max(t1 + d1, t2), max(t2 + d2, t1)
-        ans = min(solve(act + 1, r2, nt1, t2), solve(r1, act + 1, t1, nt2))
+        d1 = manhattan(but[r1], but[nb])
+        d2 = manhattan(but[r2], but[nb])
+        nt1 = t + d1
+        nt2 = d2 - t if d2 - t > 0 else 0
+        a = solve(nb, r2, nt1) + d1
+        b = solve(r1, nb, nt2) + nt2    
+        ans = a if a < b else b
     dp[k] = ans
+   
     return ans
-
-
+        
 
 def main():
-    global n, m, dp, but
+    global n, m, dp, but, state
     line = stdin.readline().strip()
-    while line != "":
-        
+    res = []
+    while line != "": 
         n, m = map(int, line.split())
-
         q = int(stdin.readline().strip())
         but = [(1, 1)]
         for _ in range(q):
             i, j = map(int, stdin.readline().split())
             but.append((i, j))
         but.append((n, m))  
+        
         dp = {}
-        ans = solve(0, 0, 0, 0) + 1
-        print(ans)
+        ans = solve(0, 0, 0)
+        res.append(str(ans))
         line = stdin.readline().strip()
+    print("\n".join(res))
 
 main()
