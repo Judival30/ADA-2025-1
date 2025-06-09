4e3b84d0748686abd52be78124cf812596683d65
Juan Diego Valencia Alomia <110737499+Judival30@users.noreply.github.com>
2025-06-09 00:23:16 -0500

    Add files via upload

--- tarea1/bit.py
+++ tarea1/bit.py
@@ -1,3 +1,10 @@
+"""
+Juan Diego Valencia Alomia
+uva: 183 Bit Maps
+
+"""
+
+
 from sys import stdin
 from sys import setrecursionlimit
 
@@ -7,48 +14,54 @@ id, n, m, ansSize = 0, 0, 0, 0
 sizebit = 0
 
 bitm = []
-tab = [["" for j in range(201)] for i in range(201)]
+tab = [[-1 for j in range(501)] for i in range(501)]
 
 
-def dfsBtoD(i1, i2, j1, j2):
-    global bitm, ansSize
+def BtoD(i1, i2, j1, j2):
+    res = []
     if not (i1 > i2 or j1 > j2) and i1 < n and i2 < n and j1 < m and j2 < m:
-        flag = True
-        i = i1
-        act = tab[i1][j1]
-        while i <= i2 and flag:
-            j = j1
-            while j <= j2 and flag:
-                if act != tab[i][j]:
-                    flag = False
-                j += 1
-            i += 1
-    
-        if flag:
-            bitm.append(act) 
-            ansSize += 1
+        if i1 == i2 and j1 == j2:
+            res.append(tab[i1][j1])
         else:
-            bitm.append("D")  
-            ansSize += 1
             mid_i = (i1 + i2) // 2
             mid_j = (j1 + j2) // 2
-            dfsBtoD(i1, mid_i, j1, mid_j)
-            dfsBtoD(i1, mid_i, mid_j + 1, j2)
-            dfsBtoD(mid_i + 1, i2, j1, mid_j)
-            dfsBtoD(mid_i + 1, i2, mid_j + 1, j2)
+            ways = [(i1, mid_i, j1, mid_j), (i1, mid_i, mid_j + 1, j2), (mid_i + 1, i2, j1, mid_j), (mid_i + 1, i2, mid_j + 1, j2)]
+            act = ""
+            i = 0
+            flag = True
+            lsts = []
+            for x1, x2, y1, y2 in ways:
+                lst = BtoD(x1, x2, y1, y2)
+                lsts.extend(lst)   
+                if i == 0:
+                    act = lst[0]
+                elif len(lst) > 0:
+                    if act != lst[0] or act == "D":
+                        flag = False
+                i += 1
+         
+            if flag and act == "1":
+                res.append("1")
+            elif flag:
+                res.append("0")
+            else:
+                res.append("D")
+                res.extend(lsts)
+
+    return res
 
 
-def dfsDtoB(i1, i2, j1, j2, b):
+def DtoB(i1, i2, j1, j2, b):
     global tab, id
     if not (i1 > i2 or j1 > j2) and i1 < n and i2 < n and j1 < m and j2 < m:
         if b[id] == "D":
             id += 1
             mid_i = (i1 + i2) // 2
             mid_j = (j1 + j2) // 2
-            dfsDtoB(i1, mid_i, j1, mid_j, b)
-            dfsDtoB(i1, mid_i, mid_j + 1, j2, b)
-            dfsDtoB(mid_i + 1, i2, j1, mid_j, b)
-            dfsDtoB(mid_i + 1, i2, mid_j + 1, j2, b)
+            DtoB(i1, mid_i, j1, mid_j, b)
+            DtoB(i1, mid_i, mid_j + 1, j2, b)
+            DtoB(mid_i + 1, i2, j1, mid_j, b)
+            DtoB(mid_i + 1, i2, mid_j + 1, j2, b)
         else:
             i = i1
             while i <= i2:
@@ -66,12 +79,11 @@ def main1():
     line = stdin.readline().strip()
     while line != "#":
         fort, n, m = line.split()
-        n, m = int(n), int(m) 
+        n, m = int(n), int(m)
         flag = True
         sizeBit = 0
         lines = []
         bitm = []
-        #print("i--------")
         while flag:
             line = stdin.readline().strip()
             tmp = line.split()
@@ -79,7 +91,6 @@ def main1():
                 flag = False
             else:
                 lines.append(line)
-            #print("f--------")
         lb = "".join(lines)
         if fort == "B":
             print("D%4d%4d" % (n, m))
@@ -89,20 +100,20 @@ def main1():
                     tab[i][j] = lb[id]
                     id += 1
             id = 0
-            dfsBtoD(0, n - 1, 0, m - 1)
+            ans1 = BtoD(0, n - 1, 0, m - 1)
             cont = 0
-            for i in range(len(bitm)):
+       
+            for i in range(len(ans1)):
                 if cont == 50:
                     cont = 0
                     print()
                 cont += 1
-                print(bitm[i], end="")
+                print(ans1[i], end="")
             print()
         elif fort == "D":
-            #print(lineD, lines) 
             print("B%4d%4d" % (n, m))
             id = 0
-            dfsDtoB(0, n - 1, 0, m - 1, lb)
+            DtoB(0, n - 1, 0, m - 1, lb)
             cont = 0
             for i in range(n):
                 for j in range(m):
